186ed17484ae7cb54a7e3e19cb81d909b0d2ca54
gauravjaat1307 <chgaurav0511@gmail.com>
2024-10-26 18:30:05 +0530

    first commit

--- app.js
+++ app.js
@@ -1,7 +1,29 @@
-document.addEventListener('DOMContentLoaded', () =>{
-const menu = document.getElementById('.menu');
+document.addEventListener('DOMContentLoaded', (e) => {
+    
+    const menuToggle = document.getElementById('menuToggle');
+    const closeMenu = document.getElementById('closeMenu');
+    const overlay = document.getElementById('overlay');
+    const dropdownToggle = document.querySelector('.dropdown-toggle');
+    const dropdownMenu = document.querySelector('.dropdown-menu');
 
+    menuToggle.addEventListener('click', () => {
+        overlay.style.display = 'flex';
+    });
 
-menu.addEventListener('click', () => {})
+    closeMenu.addEventListener('click', () => {
+        overlay.style.display = 'none';
+    });
 
-})
+     dropdownToggle.addEventListener('click', (e) => {
+         e.stopPropagation();
+         dropdownMenu.style.display = dropdownMenu.style.display === 'flex' ? 'none' : 'flex';
+     });
+
+     document.addEventListener('click', () => {
+         dropdownMenu.style.display = 'none';
+     });
+
+     dropdownMenu.addEventListener('click', (e) => {
+         e.stopPropagation();
+     });
+});
